981a998cb9f8256185fdd2198985814600012f1c
Lolu01 <jaiyeolalulu@gmail.com>
2024-08-23 23:52:25 +0000

    API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,24 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+Defines number_of_subscribers(subreddit)
 """
 
 import requests
 
 
-def top_ten(subreddit):
+def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
+    Function that queries the Reddit API and returns
+    the number of subscribers for a given subreddit.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+    res = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Mozilla/5.0"},
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        data = res.json()
+        return data.get('data').get('subscribers')
     else:
-        print(None)
+        return 0
