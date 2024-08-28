459b89b515a40873b2d6fcfc46c18248921086a0
Lolu01 <jaiyeolalulu@gmail.com>
2024-08-28 06:07:10 -0700

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyRedditApp/0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
+    
     if response.status_code == 200:
-        return 0
+        data = response.json()
 
-        data = response.json().get("data")
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data.get('data', {}).get('subscribers', 0)
     else:
-        return num_subs
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-    Uses reddit API to get 10 hot posts
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'myUserAgent/0.0.1'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    if response.status_code == 200:
-        print(None)
-        return
-
-    data = response.json().get("data").get("children")
-        posts = data['data'].get('children', [])
-        for post in posts:
-            print(post['data']['title'])
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        print(top_10_posts)
+        print(None)
