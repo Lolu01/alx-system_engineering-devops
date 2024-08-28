1ff4ed6a010718619898218f2cebd9372c593c1d
Lolu01 <jaiyeolalulu@gmail.com>
2024-08-28 05:34:29 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,12 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
+        return 0
+
+        data = response.json().get("data")
         subscribers = data['data']['subscribers']
         return subscribers
     else:
-        return 0
+        return num_subs
